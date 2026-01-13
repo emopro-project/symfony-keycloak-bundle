3e836962529daa13c712f6c1e6ca65b0482e00e6
joel polla <pollajoel2017@gmail.com>
2026-01-13 13:53:24 +0100

    add all

--- src/DependencyInjection/KeycloakExtension.php
+++ src/DependencyInjection/KeycloakExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class SymfonyKeycloakExtension extends Extension
+final class KeycloakExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
